90b941bc3f3a004c89ab4a4a39767f44e1e8d3bf
p4co <rungr4vity@gmail.com>
2023-12-14 12:40:08 -0600

    ok

--- app/src/main/java/com/example/mylist/classes/utils.kt
+++ app/src/main/java/com/example/mylist/classes/utils.kt
@@ -1,4 +1,14 @@
 package com.example.mylist.classes
 
+import android.content.Context
+import androidx.core.content.getSystemService
+
 class utils {
+
+    fun checkInternet(context: Context): Boolean {
+
+        val connectivityManager  = context.getSystemService(Context.CONNECTIVITY_SERVICE)
+
+        return true
+    }
 }
